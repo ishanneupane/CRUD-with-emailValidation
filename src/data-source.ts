ec837d63eb3d9a83e1e4ee82af1c6b78aff4269b
Ishan Neupane <ishanneupane8945@gmail.com>
2024-07-04 10:25:26 +0545

    j

--- src/data-source.ts
+++ src/data-source.ts
@@ -11,7 +11,7 @@ const connectionOptions: DataSourceOptions = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  synchronize: true,
+  synchronize: false,
   logging: false,
   entities: [
     join(parentDir, "src","entity", "*.ts"),
